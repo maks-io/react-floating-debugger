16d80a7a7b480ab4a203cf0b829d14f17c3b788d
Markus Kurzmann <markus@kurzmann.io>
2023-01-14 09:28:31 +0100

    Avoid in-corner sized container to go beyong screen size on narrow screens

--- src/components/FloatingDebuggerContainer.tsx
+++ src/components/FloatingDebuggerContainer.tsx
@@ -83,6 +83,7 @@ const getDynamicStyle = (
   inCornerOffset: number
 ): {
   width?: string;
+  maxWidth?: string;
   height?: string;
   transform?: string;
   transformOrigin?: string;
@@ -95,23 +96,28 @@ const getDynamicStyle = (
       transformOrigin: "top left",
     };
   } else if (size === "IN_CORNER") {
+    const maxWidth = `calc(100vw - ${inCornerOffset * 2}px)`;
     if (position === "TOP_LEFT") {
       return {
+        maxWidth,
         transform: `translate(${inCornerOffset}px, ${inCornerOffset}px) scale(${inCornerScale})`,
         transformOrigin: "top left",
       };
     } else if (position === "TOP_RIGHT") {
       return {
+        maxWidth,
         transform: `translate(calc(${screenWidth} - 100% - ${inCornerOffset}px), ${inCornerOffset}px) scale(${inCornerScale})`,
         transformOrigin: "top right",
       };
     } else if (position === "BOTTOM_LEFT") {
       return {
+        maxWidth,
         transform: `translate(${inCornerOffset}px, calc(100vh - 100% - ${inCornerOffset}px)) scale(${inCornerScale})`,
         transformOrigin: "bottom left",
       };
     } else {
       return {
+        maxWidth,
         transform: `translate(calc(${screenWidth} - 100% - ${inCornerOffset}px), calc(100vh - 100% - ${inCornerOffset}px)) scale(${inCornerScale})`,
         transformOrigin: "bottom left",
       };
